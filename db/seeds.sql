beebb1fcfe263264a899e4269f2d87a7208d5bb9
Saraf Nawar <s.999895831@gmail.com>
2023-02-05 19:00:29 -0500

    node modules changed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,40 @@
 -- values for the department table 
  
-INSERT INTO department (dept) VALUES ('engineering'); 
-INSERT INTO department (dept) VALUES ('finance'); 
-INSERT INTO department (dept) VALUES ('legal');
-INSERT INTO department (dept) VALUES ('training');
+INSERT INTO department
+    (name)
+VALUES
+    ('Engineering'),
+    ('Finance'),
+    ('Legal'),
+    ('Sales');
 
 -- values for the role table 
-INSERT INTO department (title, salary, dept_id) VALUES ('IT', 90000, 1);
-INSERT INTO department (title, salary, dept_id) VALUES ('Engineer', 80000, 2);
-INSERT INTO department (title, salary, dept_id) VALUES ('Accountant', 70000, 3);
-INSERT INTO department (title, salary, dept_id) VALUES ('Intern', 60000, 4); 
+INSERT INTO department 
+    (title, salary, dept_id) 
+VALUES 
+    ('Engineer', 120000, 1);
+    ('Lead Engineer', 150000, 1);
+
+    ('Lawyer', 190000, 2);
+    ('Legal Team Lead', 250000, 2);
+
+    ('Salesperson', 80000, 3); 
+    ('Sales Lead', 100000, 3);
+    
+    ('Accountant', 125000, 4);
+    ('Account Manager', 160000, 4);
+    
 
 
 -- values for the employee table
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jim", "Halpert", 1, null); 
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Dwight", "Scrute", 1, 1);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Michael", "Scott", 2, 2);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Phyllis", "Lapin-Vance", 3, null);
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Karen', 'Filipelli', 1, 1),
+    ('Michael', 'Scott', 1, NULL),
+    ('Pam', 'Beasly', 3, 2),
+    ('Kevin', 'Malone', 1, 3),
+    ('Kelly', 'Kapoor', 4, NULL),
+    ('Ryan', 'Howard', 6, 4),
+    ('Jan', 'Levinson', 5, 8),
+    ('Saraf', 'Nawar', 8, 7);
